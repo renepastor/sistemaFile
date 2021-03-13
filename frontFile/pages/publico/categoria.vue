c96d11b0012040a2a2c44bb5e21e9868e3b0d901
rpmamani <rpmamani@ypfb.gob.bo>
2021-03-13 09:45:43 -0400

    ajustes bd

--- frontFile/pages/publico/categoria.vue
+++ frontFile/pages/publico/categoria.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="pa-2">
+    <div class="pa-2">
         <v-card>
             <div class="pa-2 headline grey lighten-2  text-center">
                 LO QUE NECESITA ESTÁ AQUI
             </div>
             <v-card-text>
                 <v-container>
-                    <div class="text-right">
+                    <div class="text-center">
                         <img src="~/assets/LOGO ENTEL.png" alt="DATACOM" width="250" class="right VuetifyLogo">
                     </div>
                     <v-row>
-                        <v-col v-for="row in listServicio"
+                        <v-col v-for="row in listCategoria"
                                 :key="row.id"
-                                cols="12" sm="12" md="4" class="pa-0">
-                            <v-btn text :to="'/publico/datosContrato?'+row.id">
-                                <v-icon left>mdi-chevron-right</v-icon>{{ row.servicio }}
-                            </v-btn>
+                                cols="12" sm="12" md="4">
+                            <nuxt-link :to="'/publico/servicio?'+row.id">
+                                <v-btn text>
+                                    <v-icon left x-large>{{ row.logo }}</v-icon> {{ row.nombre }}
+                                </v-btn>
+                            </nuxt-link>
                         </v-col>
                     </v-row>
                 </v-container>
@@ -53,7 +55,7 @@ export default {
     data () {
         return {
             title:'Datos Personales',
-            listServicio:[],
+            listCategoria:[],
             desserts: [],
             modulo:{
                 nombre:this.param
@@ -62,31 +64,29 @@ export default {
         }
     },
     async asyncData({$axios}) {
-        var URLactual = window.location;
-        var arrayUri = (URLactual.href).split("?");
-        const q = {query: `{allServicios(condition:{categoriaId:"${arrayUri[1]}", estado:"C"}){
-                                nodes{id servicio}
+        const q = {query: `{allCategoriaServicios {
+                                nodes {
+                                    fechaInicio
+                                    fechaVigencia
+                                    id
+                                    logo
+                                    nombre
+                                }
                             }}`};
-        
         const datos = await $axios.$post(`${process.env.BASE_URL}/graphql`, q)
         try {
-            return {listServicio: datos.data.allServicios.nodes};
+            //this.listCategoria = datos.data.allCategoriaServicios.nodes;
+            return {listCategoria: datos.data.allCategoriaServicios.nodes};
         } catch (error) {
             console.log('Error', error)
         }
-    },
-    async created(){
-        var URLactual = window.location;
-        var arrayUri = (URLactual.href).split("?");
-        const q = {query: `{allServicios(condition:{categoriaId:"${arrayUri[1]}", estado:"C"}){
-                                nodes{id servicio}
-                            }}`};
-        const datos = await this.$axios.$post(`${process.env.BASE_URL}/graphql`, q)
+        /*const datos = await $axios.$post(`${process.env.BASE_URL}/modulos`,{headers: {'Ypfb-App-Access-Token' : process.env.API_KEY}})
+        console.log(datos)
         try {
-            this.listServicio = datos.data.allServicios.nodes;
+            return {desserts:datos.data};
         } catch (error) {
             console.log('Error', error)
-        }
+        }*/
     }
 }
 </script>
